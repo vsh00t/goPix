2ad7e01f3610a6ac722fbd49388aef92a9c630c2
vsh00t <37148657+vsh00t@users.noreply.github.com>
2022-03-16 14:57:39 -0500

    Update scanPort.go

--- handler/scanPort.go
+++ handler/scanPort.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"main/database"
 	"os/exec"
 	"strings"
@@ -40,3 +41,34 @@ func scanPort(db *sql.DB, domain string, subdomain string, host string, ip strin
 		}
 	}
 }
+
+func IsWeb(db *sql.DB, domain string, subdomain string, port string) {
+	protocol := []string{"http://", "https://"}
+	var state []string
+
+	for _, protocolo := range protocol {
+		resp, err := http.Get(protocolo + subdomain + ":" + port)
+		if err != nil {
+			state = append(state, "0")
+			break
+		}
+		defer resp.Body.Close()
+		if resp.StatusCode == 200 {
+			state = append(state, "200")
+		} else {
+			state = append(state, "0")
+		}
+	}
+	if len(state) == 2 {
+		if state[0] == "200" && state[1] == "200" {
+			database.UpdateHttp(database.ConnectDB(), subdomain, port, 1)
+		} else if state[0] == "200" && state[1] == "0" {
+			database.UpdateHttp(database.ConnectDB(), subdomain, port, 2)
+		} else if state[0] == "0" && state[1] == "200" {
+			database.UpdateHttp(database.ConnectDB(), subdomain, port, 3)
+		} else {
+			database.UpdateHttp(database.ConnectDB(), subdomain, port, 0)
+		}
+	}
+}
+
